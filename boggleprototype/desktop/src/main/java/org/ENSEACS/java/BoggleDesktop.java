6727b2a7189d62c65403eb2f89cd85ab91dfec8b
kelzzzz <cavin.a.kelsey@gmail.com>
2025-04-23 21:21:01 +0200

    Testing pre commit

--- boggleprototype/desktop/src/main/java/org/ENSEACS/java/BoggleDesktop.java
+++ boggleprototype/desktop/src/main/java/org/ENSEACS/java/BoggleDesktop.java
@@ -7,6 +7,7 @@ import org.ENSEACS.core.Boggle;
 
 public class BoggleDesktop {
 	public static void main (String[] args) {
+		System.out.println("Testing pre-commit hook");
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		new LwjglApplication(new Boggle(), config);
 	}
