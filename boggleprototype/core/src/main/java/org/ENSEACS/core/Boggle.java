5ea78510310d7a766791e1f93842193ceb19d36b
kelzzzz <cavin.a.kelsey@gmail.com>
2025-04-28 08:31:03 +0200

    #2 Moving UI logic to a new branch

--- boggleprototype/core/src/main/java/org/ENSEACS/core/Boggle.java
+++ boggleprototype/core/src/main/java/org/ENSEACS/core/Boggle.java
@@ -4,16 +4,13 @@ import com.badlogic.gdx.graphics.GL30;
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.utils.viewport.FitViewport;
-import org.ENSEACS.core.graphics.LetterTile;
 
 public class Boggle implements ApplicationListener {
 	FitViewport viewport;
-	LetterTile lt = new LetterTile();
 
 	@Override
 	public void create () {
 		viewport = new FitViewport(8,5);
-		lt.create();
 	}
 
 	@Override
