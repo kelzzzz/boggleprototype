e192213739acaa22f1e333de275d100533cbb03d
kelzzzz <cavin.a.kelsey@gmail.com>
2025-04-28 08:49:42 +0200

    #2 Just adds a logger :)

--- boggleprototype/core/src/main/java/org/ENSEACS/core/Boggle.java
+++ boggleprototype/core/src/main/java/org/ENSEACS/core/Boggle.java
@@ -3,13 +3,18 @@ package org.ENSEACS.core;
 import com.badlogic.gdx.graphics.GL30;
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.utils.Logger;
 import com.badlogic.gdx.utils.viewport.FitViewport;
+
+import static com.badlogic.gdx.Application.LOG_INFO;
 
 public class Boggle implements ApplicationListener {
 	FitViewport viewport;
+	private static final Logger LOGGER = new Logger(Boggle.class.getName(),LOG_INFO);
 
 	@Override
 	public void create () {
+		LOGGER.info("Creating main viewport and window.");
 		viewport = new FitViewport(8,5);
 	}
 
