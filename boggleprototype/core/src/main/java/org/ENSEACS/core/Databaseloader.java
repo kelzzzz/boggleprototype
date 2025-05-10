a6b4ff0076da5989f3e59b63bca32947dc40117e
edilsonalberto <edilson.marcelino04@gmail.com>
2025-05-10 18:44:38 +0200

    #6 Commit

--- boggleprototype/core/src/main/java/org/ENSEACS/core/Databaseloader.java
+++ boggleprototype/core/src/main/java/org/ENSEACS/core/Databaseloader.java
@@ -3,29 +3,35 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.sql.*;
+// EdilsonAlberto
+// Edilson Alberto - Databaseloader is a class that was created to help find first the file, in this case the dictionary
 public class Databaseloader {
-
+    // To load the file, we need a method that uses the Connection API class
     public static Connection loadToMyMemoryDB() {
+        // We first try to locate the file in the diffent folders we have
         String userDirectory = System.getProperty("user.dir");
         Path databasePath = Paths.get(userDirectory, "assets", "dictionary.db");
+        // If it isn't found on this folder, it will go one level above to try again
         if (!Files.exists(databasePath)) {
             databasePath = Paths.get(userDirectory, "..", "assets", "dictionary.db").normalize();
         }
+        // In case it doesn't find, it will return not found
         if (!Files.exists(databasePath)) {
-            throw new RuntimeException("dictionary.db file not found in:\n  " + Paths.get(userDirectory, "assets", "dictionary.db") + "\n  " + Paths.get(userDirectory, "..", "assets", "dictionary.db"));
+            throw new RuntimeException("We didn't manage to find dictionary db in\n  " + Paths.get(userDirectory, "assets", "dictionary.db") + "\n  " + Paths.get(userDirectory, "..", "assets", "dictionary.db"));
         }
+        // In case we find it, we convert to String and Connect with our Memory program
         String filePath = databasePath.toAbsolutePath().toString();
         try {
-            Connection diskConn = DriverManager.getConnection("jdbc:sqlite:" + filePath);
-            Connection memConn  = DriverManager.getConnection("jdbc:sqlite::memory:");
-
-            try (Statement st = memConn.createStatement()) {
-                st.execute("ATTACH DATABASE '" + filePath + "' AS src;");
-                st.execute("CREATE TABLE entries AS SELECT * FROM src.entries;");
-                st.execute("DETACH DATABASE src;");
+            Connection connectionofDisk = DriverManager.getConnection("jdbc:sqlite:" + filePath);
+            Connection connectionwMemory  = DriverManager.getConnection("jdbc:sqlite::memory:");
+            // We run some queries of our database
+            try (Statement something = connectionwMemory.createStatement()) {
+                something.execute("ATTACH DATABASE '" + filePath + "' AS src;");
+                something.execute("CREATE TABLE entries AS SELECT * FROM src.entries;");
+                something.execute("DETACH DATABASE src;");
             }
-            diskConn.close();
-            return memConn;
+            connectionofDisk .close();
+            return connectionwMemory;
 
         } catch (SQLException e) {
             throw new RuntimeException("Failed to load in-memory DB", e);
--- boggleprototype/core/src/main/java/org/ENSEACS/core/WordCheckerTest.java
+++ boggleprototype/core/src/main/java/org/ENSEACS/core/WordCheckerTest.java
@@ -5,7 +5,8 @@ import org.junit.jupiter.api.Test;
 import java.sql.*;
 
 import static org.junit.jupiter.api.Assertions.*;
-
+// Edilson Alberto
+// This class has our JUNIT Tests that run different test to check if our code is working perfectly
 public class WordCheckerTest {
 
     static Connection connection;
@@ -13,13 +14,18 @@ public class WordCheckerTest {
 
     @BeforeAll
     public static void setup() throws SQLException {
-
+        // We establish a Connection with the database and we call our method, LoadtoMyMEMORY
         connection = Databaseloader.loadToMyMemoryDB();
+        // This variable will help us using our Word Checker class to check certain words
         checkingword = new WordChecker(connection);
     }
     @Test
+    // This test checks if the Connection to the database is valid, in other words, if it can find the file and connect to it and also read it
+    // This test is very important, because if it doesn't pass, the other won't
+    // Because we depend on the connection to read the file
     public void testDatabaseConnectionIsValid() {
         try {
+            // We use assertTrue because we expect it to be true
             assertTrue(connection.isValid(1), "Database connection is not valid.");
             System.out.println("Connection to database is a success");
         } catch (SQLException e) {
@@ -27,19 +33,27 @@ public class WordCheckerTest {
             fail("Exception while validating connection.");
         }
     }
+    // This test uses wordchecker to test if Car is a valid word / is in the database
+    // If this test passes, this means, the word car is in the database
     @Test
     void testifCarIsValid() {
         assertTrue(checkingword.isValid("car"));
     }
+    // This test uses wordchecker to test if crazy is a valid word / is in the database
+    // If this test passes, this means, the word crazy is in the database
     @Test
     void testifCrazyIsValid(){
         assertTrue(checkingword.isValid("crazy"));
     }
+    // We test if the code can recognize words that aren't on the dictionary
+    // We expect words that are not in the dictionary, to be left out
+    // We expect the code to alert us once it detects
     @Test
     void testforInvalidWord() {
         assertFalse(checkingword.isValid("abcdefgh"));
     }
-
+    // This test makes us sure that even if you write the word with capital letter, it will still recognize the word and check
+    // if is in the database
     @Test
     public void WordCaseInsensitive() {
         try {
