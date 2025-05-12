3fc1849b0bca87246fd547b5b78431a317c254a4
kelzzzz <cavin.a.kelsey@gmail.com>
2025-05-13 00:47:58 +0200

    One more fix to make tests run with Maven.

--- boggleprototype/core/src/main/java/org/ENSEACS/core/database/Databaseloader.java
+++ boggleprototype/core/src/main/java/org/ENSEACS/core/database/Databaseloader.java
@@ -12,7 +12,6 @@ public class Databaseloader {
     public static Connection loadToMyMemoryDB() {
         String filePath = Gdx.files.internal("dictionary.db").file().getAbsolutePath();
 
-        System.out.println(filePath);
         try {
             Connection connectionofDisk = DriverManager.getConnection("jdbc:sqlite:" + filePath);
             Connection connectionwMemory  = DriverManager.getConnection("jdbc:sqlite::memory:");
