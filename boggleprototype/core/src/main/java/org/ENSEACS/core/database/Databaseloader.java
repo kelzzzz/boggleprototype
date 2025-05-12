421b6725dd5049e24faeeb3313449483a821da67
kelzzzz <cavin.a.kelsey@gmail.com>
2025-05-13 00:47:12 +0200

    Fix Junit tests and create headless backend for tests.

--- boggleprototype/core/src/main/java/org/ENSEACS/core/database/Databaseloader.java
+++ boggleprototype/core/src/main/java/org/ENSEACS/core/database/Databaseloader.java
@@ -1,35 +1,26 @@
 package org.ENSEACS.core.database;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.files.FileHandle;
+
+import java.net.URL;
+
 import java.sql.*;
 // EdilsonAlberto
 // Edilson Alberto - Databaseloader is a class that was created to help find first the file, in this case the dictionary
 public class Databaseloader {
     // To load the file, we need a method that uses the Connection API class
     public static Connection loadToMyMemoryDB() {
-        // We first try to locate the file in the diffent folders we have
-        String userDirectory = System.getProperty("user.dir");
-        Path databasePath = Paths.get(userDirectory, "assets", "dictionary.db");
-        // If it isn't found on this folder, it will go one level above to try again
-        if (!Files.exists(databasePath)) {
-            databasePath = Paths.get(userDirectory, "..", "assets", "dictionary.db").normalize();
-        }
-        // In case it doesn't find, it will return not found
-        if (!Files.exists(databasePath)) {
-            throw new RuntimeException("We didn't manage to find dictionary db in\n  " + Paths.get(userDirectory, "assets", "dictionary.db") + "\n  " + Paths.get(userDirectory, "..", "assets", "dictionary.db"));
-        }
-        // In case we find it, we convert to String and Connect with our Memory program
-        String filePath = databasePath.toAbsolutePath().toString();
+        String filePath = Gdx.files.internal("dictionary.db").file().getAbsolutePath();
+
+        System.out.println(filePath);
         try {
             Connection connectionofDisk = DriverManager.getConnection("jdbc:sqlite:" + filePath);
             Connection connectionwMemory  = DriverManager.getConnection("jdbc:sqlite::memory:");
             // We run some queries of our database
-            try (Statement something = connectionwMemory.createStatement()) {
-                something.execute("ATTACH DATABASE '" + filePath + "' AS src;");
-                something.execute("CREATE TABLE entries AS SELECT * FROM src.entries;");
-                something.execute("DETACH DATABASE src;");
-            }
+            Statement something = connectionwMemory.createStatement();
+            something.execute("ATTACH DATABASE '" + filePath + "' AS src;");
+            something.execute("CREATE TABLE entries AS SELECT * FROM src.entries;");
+            something.execute("DETACH DATABASE src;");
             connectionofDisk .close();
             return connectionwMemory;
 
--- boggleprototype/core/src/test/java/WordCheckerTest.java
+++ boggleprototype/core/src/test/java/WordCheckerTest.java
@@ -1,11 +1,11 @@
 import org.ENSEACS.core.database.Databaseloader;
 import org.ENSEACS.core.database.WordChecker;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-
 import java.sql.*;
 
-import static org.junit.jupiter.api.Assertions.*;
+
 // Edilson Alberto
 // This class has our JUNIT Tests that run different test to check if our code is working perfectly
 public class WordCheckerTest {
@@ -15,10 +15,13 @@ public class WordCheckerTest {
 
     @BeforeAll
     public static void setup() throws SQLException {
+        Headless.loadHeadless();
+
         // We establish a Connection with the database and we call our method, LoadtoMyMEMORY
         connection = Databaseloader.loadToMyMemoryDB();
         // This variable will help us using our Word Checker class to check certain words
         checkingword = new WordChecker(connection);
+
     }
     @Test
     // This test checks if the Connection to the database is valid, in other words, if it can find the file and connect to it and also read it
@@ -27,31 +30,31 @@ public class WordCheckerTest {
     public void testDatabaseConnectionIsValid() {
         try {
             // We use assertTrue because we expect it to be true
-            assertTrue(connection.isValid(1), "Database connection is not valid.");
+            Assertions.assertTrue(connection.isValid(1), "Database connection is not valid.");
             System.out.println("Connection to database is a success");
         } catch (SQLException e) {
             e.printStackTrace();
-            fail("Exception while validating connection.");
+            Assertions.fail("Exception while validating connection.");
         }
     }
     // This test uses wordchecker to test if Car is a valid word / is in the database
     // If this test passes, this means, the word car is in the database
     @Test
     void testifCarIsValid() {
-        assertTrue(checkingword.isValid("car"));
+        Assertions.assertTrue(checkingword.isValid("car"));
     }
     // This test uses wordchecker to test if crazy is a valid word / is in the database
     // If this test passes, this means, the word crazy is in the database
     @Test
     void testifCrazyIsValid(){
-        assertTrue(checkingword.isValid("crazy"));
+        Assertions.assertTrue(checkingword.isValid("crazy"));
     }
     // We test if the code can recognize words that aren't on the dictionary
     // We expect words that are not in the dictionary, to be left out
     // We expect the code to alert us once it detects
     @Test
     void testforInvalidWord() {
-        assertFalse(checkingword.isValid("abcdefgh"));
+        Assertions.assertFalse(checkingword.isValid("abcdefgh"));
     }
     // This test makes us sure that even if you write the word with capital letter, it will still recognize the word and check
     // if is in the database
@@ -63,16 +66,12 @@ public class WordCheckerTest {
             boolean found = result.next();
             result.close();
             somethingToFind.close();
-            assertTrue(found, "'Beautiful' should be found regardless of case.");
+            Assertions.assertTrue(found, "'Beautiful' should be found regardless of case.");
         } catch (SQLException e) {
             e.printStackTrace();
-            fail("It failed.");
+            Assertions.fail("It failed.");
         }
     }
-
-
-
-
 }
 
 
