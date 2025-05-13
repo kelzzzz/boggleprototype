2eae7a266b0861ffd980330be582cb022e561a18
kelzzzz <cavin.a.kelsey@gmail.com>
2025-05-13 15:50:13 +0200

    Add in player and points classes from Thomas' branch.

---
+++ boggleprototype/core/src/main/java/org/ENSEACS/core/gamelogic/Player.java
@@ -0,0 +1,22 @@
+package org.ENSEACS.core.gamelogic;
+import org.ENSEACS.core.gamelogic.PointsCalculator;
+
+import java.util.ArrayList;
+
+public class Player {
+    private int score = 0;
+    private ArrayList<String> wordsCreated = new ArrayList<>();
+
+    public void addWord(String word) {
+        this.score += PointsCalculator.calculateScore(word);
+        this.wordsCreated.add(word);
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public ArrayList<String> getWordsCreated(){
+        return this.wordsCreated;
+    }
+}
---
+++ boggleprototype/core/src/main/java/org/ENSEACS/core/gamelogic/PointsCalculator.java
@@ -0,0 +1,14 @@
+package org.ENSEACS.core.gamelogic;
+
+public class PointsCalculator {
+
+    public static int calculateScore(String word) {
+        int length = word == null ? 0 : word.length();
+        if (length < 3) return 0;
+        if (length <= 4) return 100;
+        if (length == 5) return 200;
+        if (length == 6) return 300;
+        if (length == 7) return 500;
+        return 1100;
+    }
+}
